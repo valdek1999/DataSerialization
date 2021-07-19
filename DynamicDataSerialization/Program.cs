c4a490f4954b17c4ef98a87eb73cc0fed32e436a
valdek1999 <i.irhin@yandex.ru>
2021-07-19 17:39:45 +0300

    Project has been updated

---
+++ DynamicDataSerialization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DynamicDataSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
